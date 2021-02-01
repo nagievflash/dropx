973437a3ba128750affa813c7bcc5b34b8b66ce9
Tarlan Nagiev <tarlan.nagiev@mail.ru>
2021-02-01 22:44:11 +0300

    Update 2014_10_12_000000_create_users_table.php
    change users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->increments('id');;
+            $table->string('uid');
+            $table->string('name')->nullable();
+            $table->string('username')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
